a01ecfb0b4901e54e0901d7ac1a880d20185ce27
Scott Jackson <daneren2005@gmail.com>
2013-01-25 07:22:06 -0800

    Do a null check on shuffle params

--- subsonic-android/src/github/daneren2005/dsub/util/ShufflePlayBuffer.java
+++ subsonic-android/src/github/daneren2005/dsub/util/ShufflePlayBuffer.java
@@ -108,9 +108,11 @@ public class ShufflePlayBuffer {
     private void clearBufferIfnecessary() {
         synchronized (buffer) {
 			final SharedPreferences prefs = Util.getPreferences(context);
-            if (currentServer != Util.getActiveServer(context) || !currentFolder.equals(Util.getSelectedMusicFolderId(context))
-				|| !genre.equals(prefs.getString(Constants.PREFERENCES_KEY_SHUFFLE_GENRE, "")) || !startYear.equals(prefs.getString(Constants.PREFERENCES_KEY_SHUFFLE_START_YEAR, ""))
-				|| !endYear.equals(prefs.getString(Constants.PREFERENCES_KEY_SHUFFLE_END_YEAR, ""))) {
+            if (currentServer != Util.getActiveServer(context)
+				|| (currentFolder != null && !currentFolder.equals(Util.getSelectedMusicFolderId(context)))
+				|| (genre != null && !genre.equals(prefs.getString(Constants.PREFERENCES_KEY_SHUFFLE_GENRE, "")))
+				|| (startYear != null && !startYear.equals(prefs.getString(Constants.PREFERENCES_KEY_SHUFFLE_START_YEAR, "")))
+				|| (endYear != null && !endYear.equals(prefs.getString(Constants.PREFERENCES_KEY_SHUFFLE_END_YEAR, "")))) {
                 currentServer = Util.getActiveServer(context);
 				currentFolder = Util.getSelectedMusicFolderId(context);
 				genre = prefs.getString(Constants.PREFERENCES_KEY_SHUFFLE_GENRE, "");
