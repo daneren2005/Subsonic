0ea6895860c9c8f721bae412cce353299a9ef3cc
daneren2005 <daneren2005@gmail.com>
2013-08-09 12:16:39 -0700

    Go back to putting album in since now RemoteControlClient is used for other stuff

--- src/github/daneren2005/dsub/util/compat/RemoteControlClientICS.java
+++ src/github/daneren2005/dsub/util/compat/RemoteControlClientICS.java
@@ -62,7 +62,7 @@ public class RemoteControlClientICS extends RemoteControlClientHelper {
 		// Update the remote controls
     	mRemoteControl.editMetadata(true)
     	.putString(MediaMetadataRetriever.METADATA_KEY_ARTIST, (currentSong == null) ? null : currentSong.getArtist())
-    	.putString(MediaMetadataRetriever.METADATA_KEY_ALBUM, (currentSong == null) ? null : currentSong.getArtist())
+    	.putString(MediaMetadataRetriever.METADATA_KEY_ALBUM, (currentSong == null) ? null : currentSong.getAlbum())
     	.putString(MediaMetadataRetriever.METADATA_KEY_ALBUMARTIST, (currentSong == null) ? null : currentSong.getArtist())
     	.putString(MediaMetadataRetriever.METADATA_KEY_TITLE, (currentSong) == null ? null : currentSong.getTitle())
     	.putString(MediaMetadataRetriever.METADATA_KEY_GENRE, (currentSong) == null ? null : currentSong.getGenre())
