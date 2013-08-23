ef2ce5d443891d9329b5c291fb78349c01c9fb4a
daneren2005 <daneren2005@gmail.com>
2013-08-23 10:30:42 -0700

    Fix starred items not being selectable

--- src/github/daneren2005/dsub/view/ArtistEntryView.java
+++ src/github/daneren2005/dsub/view/ArtistEntryView.java
@@ -52,6 +52,7 @@ public class ArtistEntryView extends UpdateView {
 
         titleView = (TextView) findViewById(R.id.artist_name);
 		starButton = (ImageButton) findViewById(R.id.artist_star);
+		starButton.setFocusable(false);
 		moreButton = (ImageView) findViewById(R.id.artist_more);
 		moreButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
