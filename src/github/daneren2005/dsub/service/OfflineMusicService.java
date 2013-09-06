623919201f32338efc65d70b00281be5beccf3f4
daneren2005 <daneren2005@gmail.com>
2013-09-06 16:15:26 -0700

    Update OfflineMusicService.java

--- src/github/daneren2005/dsub/service/OfflineMusicService.java
+++ src/github/daneren2005/dsub/service/OfflineMusicService.java
@@ -662,6 +662,11 @@ public class OfflineMusicService extends RESTMusicService {
     public int processOfflineSyncs(final Context context, final ProgressListener progressListener) throws Exception{
 		throw new OfflineException("Offline scrobble cached can not be processes while in offline mode");
     }
+    
+    @Override
+    public void setInstance(Integer instance) {
+    	throw new OfflinceException("Offline servers only have one instance");
+    }
 
     private void listFilesRecursively(File parent, List<File> children) {
         for (File file : FileUtil.listMediaFiles(parent)) {
