eb12a600db94d7dbc8b69cffbe1dc2fb303718cc
daneren2005 <daneren2005@gmail.com>
2013-11-26 11:29:35 -0800

    Move custom onNewIntent logic to FragActivity

--- src/github/daneren2005/dsub/activity/SubsonicActivity.java
+++ src/github/daneren2005/dsub/activity/SubsonicActivity.java
@@ -356,33 +356,6 @@ public class SubsonicActivity extends ActionBarActivity implements OnItemSelecte
 	public void onNothingSelected(AdapterView<?> parent) {
 		
 	}
-
-	@Override
-	public void onNewIntent(Intent intent) {
-		super.onNewIntent(intent);
-
-		if(currentFragment != null && currentFragment instanceof SearchFragment) {
-			String query = intent.getStringExtra(Constants.INTENT_EXTRA_NAME_QUERY);
-			boolean autoplay = intent.getBooleanExtra(Constants.INTENT_EXTRA_NAME_AUTOPLAY, false);
-			boolean requestsearch = intent.getBooleanExtra(Constants.INTENT_EXTRA_REQUEST_SEARCH, false);
-
-			if (query != null) {
-				((SearchFragment)currentFragment).search(query, autoplay);
-			} else {
-				((SearchFragment)currentFragment).populateList();
-				if (requestsearch) {
-					onSearchRequested();
-				}
-			}
-		} else if(currentFragment != null && intent.getStringExtra(Constants.INTENT_EXTRA_NAME_QUERY) != null) {
-			setIntent(intent);
-
-			SearchFragment fragment = new SearchFragment();
-			replaceFragment(fragment, currentFragment.getRootId(), fragment.getSupportTag());
-		} else {
-			setIntent(intent);
-		}
-	}
 	
 	private void populateDrawer() {
 		SharedPreferences prefs = Util.getPreferences(this);
