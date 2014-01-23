e851fcaabb00cf2570de3553fcda6003181d18e6
Scott Jackson <daneren2005@gmail.com>
2014-01-22 21:24:52 -0800

    Default to song list if album's in genre is 0

--- src/github/daneren2005/dsub/fragments/SelectDirectoryFragment.java
+++ src/github/daneren2005/dsub/fragments/SelectDirectoryFragment.java
@@ -495,6 +495,9 @@ public class SelectDirectoryFragment extends SubsonicFragment implements Adapter
 					result = service.getStarredList(context, this);
 				} else if(("genres".equals(albumListType) && Util.checkServerVersion(context, "1.10.0")) || "years".equals(albumListType)) {
 					result = service.getAlbumList(albumListType, albumListExtra, size, 0, context, this);
+					if(result.getChildrenSize() == 0 && "genres".equals(albumListType)) {
+						result = service.getSongsByGenre(albumListExtra, size, 0, context, this);
+					}
 				} else if("genres".equals(albumListType)) {
 					result = service.getSongsByGenre(albumListExtra, size, 0, context, this);
 				} else {
