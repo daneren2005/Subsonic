c9ed6303711f9799e61eb4455bbbbf21405d91d4
Scott Jackson <daneren2005@gmail.com>
2014-09-05 11:43:53 -0700

    Label any music with genre == audiobook as one as well to cover when server is not labeling them correctly

--- src/github/daneren2005/dsub/service/parser/MusicDirectoryEntryParser.java
+++ src/github/daneren2005/dsub/service/parser/MusicDirectoryEntryParser.java
@@ -71,7 +71,7 @@ public class MusicDirectoryEntryParser extends AbstractParser {
 			String type = get("type");
 			if("podcast".equals(type)) {
 				entry.setType(MusicDirectory.Entry.TYPE_PODCAST);
-			} else if("audiobook".equals(type)) {
+			} else if("audiobook".equals(type) || (entry.getGenre() != null && "audiobook".equals(entry.getGenre().toLowerCase()))) {
 				entry.setType(MusicDirectory.Entry.TYPE_AUDIO_BOOK);
 			}
         } else if(!"".equals(artist)) {
