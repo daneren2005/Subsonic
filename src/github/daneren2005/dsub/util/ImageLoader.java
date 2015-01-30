b30700dd704c49ff5229991a20369ea493e5f163
Scott Jackson <daneren2005@gmail.com>
2015-01-29 16:57:04 -0800

    Revert key back to being based off of id

--- src/github/daneren2005/dsub/util/ImageLoader.java
+++ src/github/daneren2005/dsub/util/ImageLoader.java
@@ -123,6 +123,7 @@ public class ImageLoader {
 
 			return getUnknownImage(key, size, color, null, null);
 		} else {
+			key = getKey(entry.getId() + "unknown", size);
 			String hash;
 			if(entry.getAlbum() != null) {
 				hash = entry.getAlbum();
@@ -133,7 +134,6 @@ public class ImageLoader {
 			}
 			color = COLORS[Math.abs(hash.hashCode()) % COLORS.length];
 
-			key = getKey(color + "unknown", size);
 			return getUnknownImage(key, size, color, entry.getAlbum(), entry.getArtist());
 		}
 	}
