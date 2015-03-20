e9a4a2839b205c4351cc2ade00f4e4b5efb7dc29
Mark Adamson <markadamson83@gmail.com>
2015-03-20 13:57:33 +0000

    Add option to disable 'Press back again to exit' prompt

--- src/github/daneren2005/dsub/util/Constants.java
+++ src/github/daneren2005/dsub/util/Constants.java
@@ -153,6 +153,7 @@ public final class Constants {
 	public static final String PREFERENCES_KEY_REPLAY_GAIN_TYPE= "replayGainType";
 	public static final String PREFERENCES_KEY_ALBUMS_PER_FOLDER = "albumsPerFolder";
 	public static final String PREFERENCES_KEY_CAST_PROXY = "castProxy";
+	public static final String PREFERENCES_KEY_DISABLE_EXIT_PROMPT = "disableExitPrompt";
 	
 	public static final String OFFLINE_SCROBBLE_COUNT = "scrobbleCount";
 	public static final String OFFLINE_SCROBBLE_ID = "scrobbleID";
