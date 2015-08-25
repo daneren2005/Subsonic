90f0a5e5ecbe1849752a467b9aa0fe13a274e4f7
Scott Jackson <daneren2005@gmail.com>
2015-08-24 17:30:43 -0700

    Fix changing song progress while paused not updating

--- app/src/main/java/github/daneren2005/dsub/service/DownloadService.java
+++ app/src/main/java/github/daneren2005/dsub/service/DownloadService.java
@@ -1008,6 +1008,8 @@ public class DownloadService extends Service {
 				cachedPosition = position;
 				subtractPosition = 0;
 			}
+
+			onSongProgress();
 		} catch (Exception x) {
 			handleError(x);
 		}
