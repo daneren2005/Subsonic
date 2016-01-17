0a4e2e304319bb21da877d4cf59dcdcbb386d3ad
Scott Jackson <daneren2005@gmail.com>
2016-01-17 09:14:16 -0800

    Fix clipPath not working with Hardware Acceleration before 4.3

--- app/src/main/java/github/daneren2005/dsub/view/CardView.java
+++ app/src/main/java/github/daneren2005/dsub/view/CardView.java
@@ -8,12 +8,15 @@ import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import github.daneren2005.dsub.R;
 import github.daneren2005.dsub.util.DrawableTint;
 
 public class CardView extends FrameLayout{
+	private static final String TAG = CardView.class.getSimpleName();
+
 	public CardView(Context context) {
 		super(context);
 		init(context);
@@ -37,11 +40,14 @@ public class CardView extends FrameLayout{
 
 	@Override
 	public void onDraw(Canvas canvas) {
-		Path clipPath = new Path();
-
-		float roundedDp = getResources().getDimension(R.dimen.Card_Radius);
-		clipPath.addRoundRect(new RectF(canvas.getClipBounds()), roundedDp, roundedDp, Path.Direction.CW);
-		canvas.clipPath(clipPath);
+		try {
+			Path clipPath = new Path();
+			float roundedDp = getResources().getDimension(R.dimen.Card_Radius);
+			clipPath.addRoundRect(new RectF(canvas.getClipBounds()), roundedDp, roundedDp, Path.Direction.CW);
+			canvas.clipPath(clipPath);
+		} catch(Exception e) {
+			Log.e(TAG, "Failed to clip path on canvas", e);
+		}
 		super.onDraw(canvas);
 	}
 
@@ -50,6 +56,13 @@ public class CardView extends FrameLayout{
 		setBackgroundResource(DrawableTint.getDrawableRes(context, R.attr.cardBackgroundDrawable));
 		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 			setElevation(getResources().getInteger(R.integer.Card_Elevation));
+		}
+
+		// clipPath is not supported with Hardware Acceleration before API 18
+		// http://stackoverflow.com/questions/8895677/work-around-canvas-clippath-that-is-not-supported-in-android-any-more/8895894#8895894
+		if(Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2 && isHardwareAccelerated()) {
+			Log.d(TAG, "Change to software");
+			setLayerType(View.LAYER_TYPE_SOFTWARE, null);
 		}
 	}
 }
