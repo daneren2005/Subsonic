d93a8939cf7d327ce6f82b7c093a18f3bd5377ad
Scott Jackson <daneren2005@gmail.com>
2016-02-01 11:43:12 -0800

    Fix indentions to use tabs

--- app/src/main/java/github/daneren2005/dsub/domain/SearchCritera.java
+++ app/src/main/java/github/daneren2005/dsub/domain/SearchCritera.java
@@ -27,66 +27,67 @@ import java.util.regex.Pattern;
  */
 public class SearchCritera {
 
-    private final String query;
-    private final int artistCount;
-    private final int albumCount;
-    private final int songCount;
-    private Pattern pattern;
+	private final String query;
+	private final int artistCount;
+	private final int albumCount;
+	private final int songCount;
+	private Pattern pattern;
 
-    public SearchCritera(String query, int artistCount, int albumCount, int songCount) {
-        this.query = query;
-        this.artistCount = artistCount;
-        this.albumCount = albumCount;
-        this.songCount = songCount;
-    }
+	public SearchCritera(String query, int artistCount, int albumCount, int songCount) {
+		this.query = query;
+		this.artistCount = artistCount;
+		this.albumCount = albumCount;
+		this.songCount = songCount;
+	}
 
-    public String getQuery() {
-        return query;
-    }
+	public String getQuery() {
+		return query;
+	}
 
-    public int getArtistCount() {
-        return artistCount;
-    }
+	public int getArtistCount() {
+		return artistCount;
+	}
 
-    public int getAlbumCount() {
-        return albumCount;
-    }
+	public int getAlbumCount() {
+		return albumCount;
+	}
 
-    public int getSongCount() {
-        return songCount;
-    }
+	public int getSongCount() {
+		return songCount;
+	}
 
-    /**
-     * Returns and caches a pattern instance that can be used to check if a
-     * string matches the query.
-     */
-    public Pattern getPattern() {
+	/**
+	 * Returns and caches a pattern instance that can be used to check if a
+	 * string matches the query.
+	 */
+	public Pattern getPattern() {
 
-      // If the pattern wasn't already cached, create a new regular expression
-      // from the search string :
-      //  * Surround the search string with ".*" (match anything)
-      //  * Replace spaces and wildcard '*' characters with ".*"
-      //  * All other characters are properly quoted
-      if (this.pattern == null) {
-        String regex = ".*";
-        String currentPart = "";
-        for (int i = 0; i < query.length(); i++) {
-          char c = query.charAt(i);
-          if (c == '*' || c == ' ') {
-            regex += Pattern.quote(currentPart);
-            regex += ".*";
-            currentPart = "";
-          } else {
-            currentPart += c;
-          }
-        }
-        if (currentPart.length() > 0) {
-          regex += Pattern.quote(currentPart);
-        }
-        regex += ".*";
-        this.pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-      }
+		// If the pattern wasn't already cached, create a new regular expression
+		// from the search string :
+		//  * Surround the search string with ".*" (match anything)
+		//  * Replace spaces and wildcard '*' characters with ".*"
+		//  * All other characters are properly quoted
+		if (this.pattern == null) {
+			String regex = ".*";
+			String currentPart = "";
+			for (int i = 0; i < query.length(); i++) {
+				char c = query.charAt(i);
+				if (c == '*' || c == ' ') {
+					regex += Pattern.quote(currentPart);
+					regex += ".*";
+					currentPart = "";
+				} else {
+					currentPart += c;
+				}
+			}
+			if (currentPart.length() > 0) {
+				regex += Pattern.quote(currentPart);
+			}
 
-      return this.pattern;
-    }
+			regex += ".*";
+			this.pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+		}
+
+		return this.pattern;
+	}
 }
