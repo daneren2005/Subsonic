fc4a916e766ac7f6c5893c823dec05bfc2ed963e
Scott Jackson <daneren2005@gmail.com>
2016-02-04 17:45:10 -0800

    Sort music folders

--- app/src/main/java/github/daneren2005/dsub/domain/MusicFolder.java
+++ app/src/main/java/github/daneren2005/dsub/domain/MusicFolder.java
@@ -18,7 +18,12 @@
  */
 package github.daneren2005.dsub.domain;
 
+import android.util.Log;
+
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Represents a top level directory in which music or other media is stored.
@@ -27,31 +32,49 @@ import java.io.Serializable;
  * @version $Id$
  */
 public class MusicFolder implements Serializable {
-
-    private String id;
-    private String name;
+	private static final String TAG = MusicFolder.class.getSimpleName();
+	private String id;
+	private String name;
 	private boolean enabled;
 
 	public MusicFolder() {
 
 	}
-    public MusicFolder(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+	public MusicFolder(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
 	public boolean getEnabled() {
 		return enabled;
+	}
+
+	public static class MusicFolderComparator implements Comparator<MusicFolder> {
+		public int compare(MusicFolder lhsMusicFolder, MusicFolder rhsMusicFolder) {
+			if(lhsMusicFolder == rhsMusicFolder || lhsMusicFolder.getName().equals(rhsMusicFolder.getName())) {
+				return 0;
+			} else {
+				return lhsMusicFolder.getName().compareToIgnoreCase(rhsMusicFolder.getName());
+			}
+		}
+	}
+
+	public static void sort(List<MusicFolder> musicFolders) {
+		try {
+			Collections.sort(musicFolders, new MusicFolderComparator());
+		} catch (Exception e) {
+			Log.w(TAG, "Failed to sort music folders", e);
+		}
 	}
 }
