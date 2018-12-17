cc1f8935fe25363dc80bb930167ee42c91404a46
Daniel Bowman <daniel@danielbowman.co.uk>
2018-12-17 14:21:22 +0000

    Lint: Memory allocation within drawing

--- app/src/main/java/github/vrih/xsub/view/CardView.java
+++ app/src/main/java/github/vrih/xsub/view/CardView.java
@@ -3,6 +3,7 @@ package github.vrih.xsub.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Path;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
@@ -15,6 +16,9 @@ import github.vrih.xsub.util.DrawableTint;
 
 public class CardView extends MaterialCardView {
 	private static final String TAG = CardView.class.getSimpleName();
+	Path clipPath;
+	Rect bounds;
+	RectF roundedRect;
 
 	public CardView(Context context) {
 		super(context);
@@ -40,9 +44,10 @@ public class CardView extends MaterialCardView {
 	@Override
 	public void onDraw(Canvas canvas) {
 		try {
-			Path clipPath = new Path();
 			float roundedDp = getResources().getDimension(R.dimen.Card_Radius);
-			clipPath.addRoundRect(new RectF(canvas.getClipBounds()), roundedDp, roundedDp, Path.Direction.CW);
+			canvas.getClipBounds(bounds);
+			roundedRect.set(bounds);
+			clipPath.addRoundRect(roundedRect, roundedDp, roundedDp, Path.Direction.CW);
 			canvas.clipPath(clipPath);
 		} catch(Exception e) {
 			Log.e(TAG, "Failed to clip path on canvas", e);
@@ -52,6 +57,9 @@ public class CardView extends MaterialCardView {
 
 	private void init(Context context) {
 		setClipChildren(true);
+		clipPath = new Path();
+		bounds = new Rect();
+		roundedRect = new RectF();
 		setBackgroundResource(DrawableTint.getDrawableRes(context, R.attr.cardBackgroundDrawable));
 		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 			setElevation(getResources().getInteger(R.integer.Card_Elevation));
